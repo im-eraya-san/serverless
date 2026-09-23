54234510cfbcaa1ac53469e5b44e7540b2018488
im-eraya-san <anuj.9m.dubey@gmail.com>
2026-09-23 10:32:16 +0000

    adding quote in vars/tag.groovy

--- vars/tag.groovy
+++ vars/tag.groovy
@@ -2,7 +2,7 @@ def call(String LOCAL_IMAGE_NAME){
     withCredentials([
         string(credentialsId: 'AC-ID', variable: 'imgName')
     ]){ 
-        sh "echo $LOCAL_IMAGE_NAME $imgName
+        sh "echo $LOCAL_IMAGE_NAME $imgName"
         sh "docker image tag ${LOCAL_IMAGE_NAME} \${imgName}:${env.BUILD_ID}"
     }
 }
